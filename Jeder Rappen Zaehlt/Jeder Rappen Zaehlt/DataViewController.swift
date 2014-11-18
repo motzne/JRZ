bd66d90622af3e8fb0ddef43993dfcc29b169660
Tom Enz <tom@Blood-Sand.local>
2014-11-18 14:30:36 +0100

    Inital

---
+++ Jeder Rappen Zaehlt/Jeder Rappen Zaehlt/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  Jeder Rappen Zaehlt
+//
+//  Created by 2MT on 18.11.14.
+//  Copyright (c) 2014 Erni Simon & Enz Tom. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
