07c00bc868fd740616e31b5f8cf36e392003cb0b
Simon Erni <erni.simon@gmail.com>
2014-12-01 18:35:03 +0100

    Added DataTransmitter

---
+++ Jeder Rappen Zaehlt/DataTransmitter.swift
@@ -0,0 +1,16 @@
+//
+//  DataTransmitter.swift
+//  Jeder Rappen Zaehlt
+//
+//  Created by Simon Erni on 24.11.14.
+//  Copyright (c) 2014 Erni Simon & Enz Tom. All rights reserved.
+//
+
+import UIKit
+
+class DataTransmitter: NSObject {
+    
+    func newUser(vorname: String, geschlecht: Bool, alter : Int, kanton: String) {
+        
+    }
+}
